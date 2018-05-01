60d7546db4d85ead6a9841b290b6314300f044c2
zhangxishuo <zhangxishuo1998@gmail.com>
2018-05-01 21:12:21 +0800

    完成专业管理模块

--- thinkphp/application/index/controller/SpecialtyController.php
+++ thinkphp/application/index/controller/SpecialtyController.php
@@ -2,6 +2,9 @@
 
 namespace app\index\controller;
 
+use app\index\model\Specialty;
+use app\index\utils\Status;
+
 /**
  * 专业管理控制器
  */
@@ -12,5 +15,50 @@ class SpecialtyController extends BaseController
         $specialty = $this->specialtyService->getAllSpecialty();
         $this->assign('specialty', $specialty);
         return $this->fetch();
+    }
+
+    public function add()
+    {
+        $specialty = new Specialty();
+        $specialty->id = 0;
+        $specialty->name = '';
+        $this->assign('specialty', $specialty);
+        return $this->fetch();
+    }
+
+    public function save()
+    {
+        $name = $this->request->post('name');
+        $result = $this->specialtyService->save($name);
+        if (Status::isSuccess($result['status'])) {
+            $this->success($result['info'], 'specialty/index');
+        }
+    }
+
+    public function edit()
+    {
+        $id = $this->request->param('id');
+        $specialty = Specialty::get($id);
+        $this->assign('specialty', $specialty);
+        return $this->fetch('specialty/add');
+    }
+
+    public function update()
+    {
+        $id = $this->request->param('id');
+        $name = $this->request->post('name');
+        $result = $this->specialtyService->update($id, $name);
+        if (Status::isSuccess($result['status'])) {
+            $this->success($result['info'], 'specialty/index');
+        }
+    }
+
+    public function delete()
+    {
+        $id = $this->request->param('id');
+        $result = $this->specialtyService->delete($id);
+        if (Status::isSuccess($result['status'])) {
+            $this->success($result['info'], 'specialty/index');
+        }
     }
 }
--- thinkphp/application/index/service/SpecialtyService.php
+++ thinkphp/application/index/service/SpecialtyService.php
@@ -3,6 +3,7 @@
 namespace app\index\service;
 
 use app\index\model\Specialty;
+use app\index\utils\Status;
 
 /**
  * 专业服务
@@ -26,5 +27,28 @@ class SpecialtyService implements Service
     public function getAllSpecialty()
     {
         return Specialty::all();
+    }
+
+    public function save($name)
+    {
+        $specialty = new Specialty();
+        $specialty->name = $name;
+        $specialty->save();
+        return Status::getSuccessResult("保存成功");
+    }
+
+    public function update($id, $name)
+    {
+        $specialty = Specialty::get($id);
+        $specialty->name = $name;
+        $specialty->save();
+        return Status::getSuccessResult("更新成功");
+    }
+
+    public function delete($id)
+    {
+        $specialty = Specialty::get($id);
+        $specialty->delete();
+        return Status::getSuccessResult("删除成功");
     }
 }
