9b0c0d74a608ccd64284d30bca91047405613935
zhangxishuo <zhangxishuo1998@Gmail.com>
2018-06-07 16:31:23 +0800

    '培养计划首页'

--- thinkphp/application/index/model/Train.php
+++ thinkphp/application/index/model/Train.php
@@ -10,4 +10,16 @@ class Train extends Model
     {
         return $this->belongsTo('Specialty');
     }
+
+    public function getAllCourses()
+    {
+        $trainId = $this->id;
+        $trainCourses = TrainCourse::where('train_id', '=', $trainId)->select();
+        $courses = [];
+        foreach ($trainCourses as $trainCourse) {
+            $course = Course::get($trainCourse->course_id);
+            array_push($courses, $course);
+        }
+        return $courses;
+    }
 }
