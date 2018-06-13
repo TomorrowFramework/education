0ba634adf55ebefa062ba7f16e64d298008823b8
zhangxishuo <zhangxishuo1998@Gmail.com>
2018-06-13 22:24:15 +0800

    '修改日历为日期'

--- orm/src/main/java/com/tomorrow/orm/entity/Term.java
+++ orm/src/main/java/com/tomorrow/orm/entity/Term.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Calendar;
+import java.sql.Date;
 
 /**
  * @author zhangxishuo on 2018/4/30
@@ -20,9 +20,9 @@ public class Term {
 
     private String name;                     // 学期名称
 
-    private Calendar startTime;              // 起始时间
+    private Date startTime;                  // 起始时间
 
-    private Calendar endTime;                // 结束时间
+    private Date endTime;                    // 结束时间
 
     private Boolean isCurrent;               // 是否为当前学期
 }
